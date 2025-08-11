401fbfcbba2b81943637144be3d75a654a7cd77f
Auco77 <allcOliveira77@gmail.com>
2025-08-11 17:13:40 -0300

    Add project files.

---
+++ Receive/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost", UserName = "user01", Password = "auco^(x@user01" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+//Note that we declare the queue here as well. Because we might start the consumer before the publisher,
+//we want to make sure the queue exists before we try to consume messages from it.
+await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+Console.WriteLine(" 🥱 Waiting for Messages");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += (model, ea) =>
+{
+	var body = ea.Body.ToArray();
+	var message = Encoding.UTF8.GetString(body);
+	Console.WriteLine($" [x] Received {message}");
+	return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync("hello", autoAck:true, consumer: consumer);
+
+Console.WriteLine(" Press [Enter] to exit.");
+Console.ReadLine();
---
+++ Send/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost", UserName = "user01", Password = "auco^(x@user01" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var hasMoreMessage = true;
+
+while (hasMoreMessage)
+{
+	Console.WriteLine("Write a Message to Send");
+	var newMsg = Console.ReadLine();
+
+	if (newMsg != null)
+	{
+		var msgBody = Encoding.UTF8.GetBytes($"{DateTime.Now:HH:mm:ss} => {newMsg}");
+		await channel.BasicPublishAsync(exchange: String.Empty, routingKey: "hello", body: msgBody);
+		Console.WriteLine("Message Sent");
+	}
+
+	Console.WriteLine("Do you want send more message (Y/n)");
+	hasMoreMessage = Console.ReadLine()?.ToUpper() != "N";
+}
+
+Console.WriteLine(" Press [Enter] to exit");
+Console.ReadLine();
+
